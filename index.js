98d2a107ac1479373801459c0900e5be8f8d4f16
Md shaharul Islam <shaharulislam025@gmail.com>
2024-08-18 08:44:00 +0600

    change client side link

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://job-task2-client.web.app"],
+    origin: ['https://job-task2-client.web.app'],
     optionsSuccessStatus: 200,
   })
 );
