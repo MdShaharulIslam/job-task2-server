3dca000a6ea3b54f8cae042ba155201e88bfdc12
Md shaharul Islam <shaharulislam025@gmail.com>
2024-08-18 13:10:54 +0600

    set origin

--- index.js
+++ index.js
@@ -3,20 +3,16 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: '["http://localhost:5173", "https://job-task2-client.web.app/"]',
+  origin: ['http://localhost:5173', 'https://job-task2-client.web.app'], // Correctly formatted array of origins
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
-  }));
+}));
 app.use(express.json());
-
-
-
 
 const uri = process.env.DB_url;
 
@@ -31,12 +27,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const productCollection = client.db('Products').collection('product');
 
-    const productCollection = client.db('Products').collection('product')
-
-
-
-    
     app.get('/product', async (req, res) => {
       const page = parseInt(req.query.page) || 1;
       const search = req.query.search || "";
@@ -46,38 +38,38 @@ async function run() {
       const minimum = parseFloat(req.query.minimum) || 0;
       const maximum = parseFloat(req.query.maximum) || Number.MAX_VALUE;
       const limit = 11;
-    
+
       const query = {};
-    
+
       if (search) {
         query.productName = { $regex: search, $options: "i" };
       }
-    
+
       if (brand) {
         query.brand = { $regex: brand, $options: "i" };
       }
-    
+
       if (category) {
         query.category = { $regex: category, $options: "i" };
       }
-    
+
       if (!isNaN(minimum) || !isNaN(maximum)) {
         query.price = {
           $gte: minimum,
           $lte: maximum
         };
       }
-    
-      let sort = { createdAt: -1 }; 
+
+      let sort = { createdAt: -1 };
       if (sortValue === 'Low to High') {
         sort = { price: 1, createdAt: -1 };
       } else if (sortValue === 'High to Low') {
         sort = { price: -1, createdAt: -1 };
       }
-            const skip = (page - 1) * limit;
+      const skip = (page - 1) * limit;
       const result = await productCollection.find(query).sort(sort).skip(skip).limit(limit).toArray();
       const totalProducts = await productCollection.countDocuments(query);
-    
+
       res.send({
         data: result,
         currentPage: page,
@@ -85,26 +77,18 @@ async function run() {
         totalProducts
       });
     });
-    
-
-
-
-
-
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-
+    // Optionally, close the client connection here
   }
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("server is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`server is running on ${port}`);
-  });
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
