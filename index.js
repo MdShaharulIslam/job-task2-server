670397f7808702431ca5749a85f135ec165509c4
Md shaharul Islam <shaharulislam025@gmail.com>
2024-08-28 08:41:57 +0600

    cors use

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(cors());
 // Middleware
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://job-task2-client.web.app");
